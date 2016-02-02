e596b900f0870b3e6eb393411bab04f9ef4edaa0
Daniel <danieltos.bouchard@gmail.com>
2016-02-02 14:17:47 -0600

    Update l_scrn.js

--- l_scrn.js
+++ l_scrn.js
@@ -1,3 +1,16 @@
-$(document).ready( function() {
+function center_content() {
+  var width = $("body").css("width");
+  var bar_width = $("#side-bar").css("width");
+  var content_width = $("#content").css("width");
+  var margin = (width - bar_width - content_width) / 2;
+  
+  if(margin >= 0) {
+    $("#content").css("margin-left", margin);
+  }
+}
 
+$(document).ready( function() {
+  center_content();
+  
+  $(window).resize( center_content );
 });
