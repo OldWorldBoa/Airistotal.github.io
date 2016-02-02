2264bc4e9cd4a8dbc2d7c29af4e0cb592c7fcca2
Daniel <danieltos.bouchard@gmail.com>
2016-02-02 15:56:21 -0800

    Update menu.js

--- menu.js
+++ menu.js
@@ -1,8 +1,8 @@
 $(document).ready( function() {
   $(".menu-open").click( function() {
-    var visibility = $(".menu-circle").css("visibility");
+    var opacity = parseInt($(".menu-circle").css("opacity"));
     
-    if(visibility == "hidden") {
+    if(opacity == 0) {
       $(".menu-circle").animate({opacity: 1.0}, 200);
     } else {
       $(".menu-circle").animate({opacity: 0.0}, 200);
