d4b8fd70844ba1b1ffde0f206b0b7ec19b5a35ec
Daniel <danieltos.bouchard@gmail.com>
2016-02-01 22:03:10 -0800

    Update script.js

--- script.js
+++ script.js
@@ -4,4 +4,16 @@ $(document).ready( function() {
   $("#about").click(function() {
     $("#content").load("about.html");
   });
+  
+  $("#contract").click(function() {
+    $("#content").load("contract.html");
+  });
+  
+  $("#news").click(function() {
+    $("#content").load("news.html");
+  });
+  
+  $("#contact").click(function() {
+    $("#content").load("contact.html");
+  });
 });
