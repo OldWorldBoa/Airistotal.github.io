381637ba9ff0a90c8ec14ed3829c0fb127e70bd4
Daniel <danieltos.bouchard@gmail.com>
2016-02-02 15:36:42 -0800

    Update links.js

--- links.js
+++ links.js
@@ -1,19 +1,19 @@
 $(document).ready( function() {
   $("#content").load("about.html");
   
-  $("#about").click(function() {
+  $(".about").click(function() {
     $("#content").load("about.html");
   });
   
-  $("#contract").click(function() {
+  $(".contract").click(function() {
     $("#content").load("contract.html");
   });
   
-  $("#news").click(function() {
+  $(".news").click(function() {
     $("#content").load("news.html");
   });
   
-  $("#contact").click(function() {
+  $(".contact").click(function() {
     $("#content").load("contact.html");
   });
 });
