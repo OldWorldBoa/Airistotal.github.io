2b2cdd833dded8611cc5d1412462e794fef0851b
Daniel <danieltos.bouchard@gmail.com>
2016-02-02 15:43:46 -0800

    Update menu.js

--- menu.js
+++ menu.js
@@ -1,1 +1,11 @@
-
+$(document).ready( function() {
+  $(".menu-open").click( function() {
+    var visibility = $(".menu-circle").css("visibility");
+    
+    if(visibility == "hidden") {
+      $(".menu-circle").css("visibility", "visible");
+    } else {
+      $(".menu-circle").css("visibility", "hidden");
+    }
+  });
+});
