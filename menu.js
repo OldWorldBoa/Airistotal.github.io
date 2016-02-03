fefc2c71ac06193c9454d1f572bfb2b2222f79db
Daniel <danieltos.bouchard@gmail.com>
2016-02-02 20:55:38 -0800

    Update menu.js

--- menu.js
+++ menu.js
@@ -11,10 +11,13 @@ function toggle_menu() {
 $(document).ready( function() {
   $(".menu-open").on("tap click", function(e) {
     toggle_menu();
+  });
+  
+  $(".menu-open, .menu-circle, .phone-menu").on("mouseover", function(e) {
     e.stopPropagation();
   });
   
-  $(":not(.menu-open, .menu-circle, .phone-menu)").on("tap click", function(e) {
+  $(":not(.menu-open, .menu-circle, .phone-menu)").on("mouseover", function(e) {
     console.log(e.target.class);
     $(".menu-circle").animate({opacity: 0.0}, 200);
   });
