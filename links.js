1c98ab2e695bdb854240f33094148b71753ee056
Daniel <danieltos.bouchard@gmail.com>
2016-02-02 22:03:45 -0800

    Update links.js

--- links.js
+++ links.js
@@ -1,24 +1,19 @@
 $(document).ready( function() {
-  $("#content").load("about.html");
-  size_body();
+  $("#content").load("about.html", size_body);
   
   $(".about").on("tap click", function() {
-    $("#content").load("about.html");
-    size_body();
+    $("#content").load("about.html", size_body);
   });
   
   $(".contract").on("tap click", function() {
-    $("#content").load("contract.html");
-    size_body();
+    $("#content").load("contract.html", size_body);
   });
   
   $(".news").on("tap click", function() {
-    $("#content").load("news.html");
-    size_body();
+    $("#content").load("news.html", size_body);
   });
   
   $(".contact").on("tap click", function() {
-    $("#content").load("contact.html");
-    size_body();
+    $("#content").load("contact.html", size_body);
   });
 });
